999cfa7a1348fa916b3e49f5449855a654237e91
IBagurtsev <shandro94@mail.ru>
2018-10-23 15:32:23 +0300

    #11 implement json(categories, workflows), bind(ent, stat)

---
+++ src/main/scala/ru/sber/cb/ap/gusli/actor/distrib/CategoryToDisrtib.scala
@@ -0,0 +1,110 @@
+package ru.sber.cb.ap.gusli.actor.distrib
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path}
+
+import akka.actor.{ActorRef, Props}
+import play.api.libs.json.Json
+import ru.sber.cb.ap.gusli.actor.{BaseActor, Response}
+import ru.sber.cb.ap.gusli.actor.core.dto.{CategoryDto, WorkflowDto}
+import ru.sber.cb.ap.gusli.actor.ctl.model.{Category, Wf}
+import ru.sber.cb.ap.gusli.actor.ctl.model.CtlJsonProtocol.{catWrites, wfWrites}
+import FolderNames._
+import ru.sber.cb.ap.gusli.actor.distrib.CategoryToDisrtib.CategoryWritten
+
+object CategoryToDisrtib {
+  
+  def apply(path: Path, fullCatName: String, cDto: CategoryDto, receiver: ActorRef): Props =
+    Props(new CategoryToDisrtib(path, fullCatName, cDto, receiver))
+  
+  case class CategoryWritten(cDto: CategoryDto) extends Response
+  
+}
+
+/**
+  *
+  * @param artifactPath path to artifact folder
+  * @param fullCatName name formated as "branch-root/.../grandParent/parent/this"
+  * @param cDto CategoryDto
+  * @param receiver whom to send CategoryWritten Response
+  */
+class CategoryToDisrtib(artifactPath: Path, fullCatName: String, cDto: CategoryDto, receiver: ActorRef) extends BaseActor {
+  
+  private val childrenCount = cDto.subcategories.size
+  private var writtenChildrenCount = 0
+  
+  override def preStart(): Unit = {
+    writeToDisk()
+    writeChildren()
+    checkFinish()
+  }
+  
+  override def receive: Receive = {
+    case CategoryWritten(_) =>
+      writtenChildrenCount += 1
+      checkFinish()
+  }
+  
+  private def writeToDisk(): Unit = {
+    writeCategory()
+    writeWorkflows()
+  }
+  
+  private def writeChildren(): Unit = {
+    cDto.subcategories.foreach(c =>
+      context.actorOf(CategoryToDisrtib(artifactPath, fullCatName + s"/${c.name}", c, self)))
+  }
+  
+  private def writeCategory(): Path = {
+    val fileName = makeNormalJsonName(fullCatName)
+    val jsEntity = Json.prettyPrint(Json.toJson(Category.namedCategory(fullCatName)))
+    val writePath = artifactPath.resolve(apCtl).resolve(categories)resolve(if (cDto.isDeleted) delete else create)
+    Files.createDirectories(writePath)
+    Files.write(writePath.resolve(fileName), jsEntity.getBytes(StandardCharsets.UTF_8))
+  }
+  
+  private def writeWorkflows(): Unit = {
+    cDto.workflows.foreach{ wf =>
+      val wfFileName = makeNormalJsonName(wf.name)
+      val wfPath = artifactPath.resolve(apCtl).resolve(workflows).resolve(if (wf.isDeleted) delete else create)
+      writeWorkflowJson(wfPath, wf, wfFileName)
+      writeHdfs(wf)
+      if (!wf.isDeleted) {
+        writeEntityBind(wfPath, wf, wfFileName)
+        writeStatBind(wfPath, wf, wfFileName)
+      }
+    }
+  }
+  
+  private def writeWorkflowJson(wfPath: Path, wf: WorkflowDto, fileName: String) = {
+    val js = Json.prettyPrint(Json.toJson(Wf.fromDto(fullCatName, wf)))
+    Files.createDirectories(wfPath)
+    Files.write(wfPath.resolve(fileName), js.getBytes(StandardCharsets.UTF_8))
+  }
+  
+  private def writeHdfs(wDto: WorkflowDto): Unit = {
+  
+  }
+  
+  private def writeEntityBind(path: Path, wDto: WorkflowDto, wfFileName: String) =
+    writeSet(path, wDto.entities, wfFileName + ".bind")
+  
+  private def writeStatBind(path: Path, wDto: WorkflowDto, wfFileName: String) =
+    writeSet(path, wDto.stats, wfFileName + ".stat")
+  
+  private def writeSet(path: Path, set: Set[Long], fileName: String) = {
+    if (set.nonEmpty) {
+      val s = set.mkString("\n")
+      Files.write(path.resolve(fileName), s.getBytes(StandardCharsets.UTF_8))
+    }
+  }
+  
+  private def makeNormalJsonName(name: String): String = name.replace("/", "~").replace(":", "%3A").trim + ".json"
+  
+  private def checkFinish(): Unit = if (writtenChildrenCount == childrenCount) finish()
+  
+  private def finish(): Unit = {
+    receiver ! CategoryWritten(cDto)
+    context.stop(self)
+  }
+}
---
+++ src/test/scala/ru/sber/cb/ap/gusli/actor/distrib/CategoryToDisrtibTest.scala
@@ -0,0 +1,39 @@
+package ru.sber.cb.ap.gusli.actor.distrib
+
+import java.nio.file.Paths
+
+import ru.sber.cb.ap.gusli.actor.core.ActorBaseTest
+import ru.sber.cb.ap.gusli.actor.core.dto.{CategoryDto, WorkflowDto}
+import ru.sber.cb.ap.gusli.actor.distrib.CategoryToDisrtib.CategoryWritten
+
+class CategoryToDisrtibTest extends ActorBaseTest("CategoryToDisrtib") {
+  val path = Paths.get("./target/mkdistribtest/cat-create-test/")
+  val cDto = createCategoryTree()
+  
+  "CategoryToDistrib" when {
+    "preStart" should {
+      "create json entities in distrib" in {
+        system.actorOf(CategoryToDisrtib(path, cDto.name, cDto, self))
+        expectMsg(CategoryWritten(cDto))
+        import FolderNames._
+        assert(path.resolve(apCtl).resolve(categories).resolve(create).toFile.listFiles.size == 6)
+        assert(path.resolve(apCtl).resolve(workflows).resolve(create).toFile.listFiles.size == 7)
+      }
+    }
+  }
+  
+  
+  private def createCategoryTree(): CategoryDto = {
+    val wf11 = WorkflowDto("1", Map.empty, entities = Set(1, 2, 3))
+    val wf12 = WorkflowDto("2", Map.empty, entities = Set(1, 2, 3))
+    val wf12211 = WorkflowDto("3", Map.empty, entities = Set(1, 2, 3), stats = Set(1, 2, 3))
+    
+    val c1221 = CategoryDto("1221", workflows = Set(wf12211))
+    val c122 = CategoryDto("122", subcategories = Set(c1221))
+    val c121 = CategoryDto("121")
+    val c11 = CategoryDto("11")
+    val c12 = CategoryDto("12", subcategories = Set(c121, c122))
+    val c1 = CategoryDto("1", subcategories = Set(c11, c12), workflows = Set(wf11, wf12))
+    c1
+  }
+}
